d25b6f56139ba2ca88b116c1a05d7a80bb542a9f
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-01-18 11:29:24 +0100

    exercise finished

--- main.js
+++ main.js
@@ -11,5 +11,47 @@ Per fare questo bonus potremmo aver bisogno di del metodo string.includes() :occ
 Buon Lavoro e buon divertimento! :sorridere:
 
         MINI TASK
+        - Selezionare il contenitore: Recupera un elemento <div> dalla pagina e aggiungi una classe per lo stile.
+        - Creare la lampadina: Crea un elemento <img> e aggiungilo al contenitore con l'immagine di una lampadina spenta.
+        - Creare il bottone: Crea un bottone con il testo "accendi" e aggiungilo al contenitore.
+        - Implementare la funzione della lampadina: Aggiungi un evento onclick al bottone per alternare tra lampadina accesa e spenta.
+        - Cambiare stile dinamicamente: Modifica il colore di sfondo del contenitore in base allo stato della lampadina.
+
 */
+const contenitore = document.querySelector('div'); 
+contenitore.classList.add('stileContenitore') 
+
+const lampadina = document.createElement('img');
+contenitore.appendChild(lampadina)
+lampadina.setAttribute('src', './img/white_lamp.png');
+
+
+const bottone = document.createElement('button');
+contenitore.appendChild(bottone)
+bottone.textContent = 'accendi'
+bottone.classList.add('btn')
+
+
+bottone.onclick = function (){
+    if(lampadina.src.includes('white_lamp.png')) {
+        lampadina.src = './img/yellow_lamp.png';
+        bottone.textContent = 'spegni'
+        contenitore.style.backgroundColor = 'black'
+
+    }else {
+        lampadina.src = './img/white_lamp.png';
+        bottone.textContent = 'accendi'
+        contenitore.style.backgroundColor = 'white'
+    }
+
+}
+
+
+
+
+
+
+
+
+
 
